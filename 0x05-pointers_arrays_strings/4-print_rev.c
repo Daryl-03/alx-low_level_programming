72e664737ae73c9e7dab24713bc2e9b352c53167
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-05-30 01:55:04 +0000

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,23 +7,11 @@
 */
 void print_rev(char *s)
 {
-	int i;
+	int i, j;
 
-	for (i = _strlen(s) - 1; i >= 0; i--)
+	for (j = 0; s[j] != '\0'; j++)
+		;
+	for (i = j - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to be checked
- * Return: int length of string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i);
 }
