5d446eb9d12642c0f63e9bf46acfff4bf658f308
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-05-30 01:07:18 +0000

    sm fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,6 +11,6 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n || i==0; i++)
-		printf("%d%s", a[i], i == n - 1 ? "\n" : ", ");
+	for (i = 0; i < n || i == 0; i++)
+		printf("%d%s", a[i], i >= n - 1 ? "\n" : ", ");
 }
