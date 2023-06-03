422ec0ba95f763a501996768f1830a99a69ba62d
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-06-03 00:48:38 +0000

    betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (destLength = 0; dest[destLength] != '\0'; destLength++)
 		;
-	for (j = 0; src[j] != '\0' && j<n; j++)
+	for (j = 0; src[j] != '\0' && j < n; j++)
 		dest[destLength + j] = src[j];
 	dest[destLength + j] = '\0';
 
