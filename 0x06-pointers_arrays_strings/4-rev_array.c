cd5a98e105510ae433f61467b1aafb0229a40ed3
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-06-03 01:59:57 +0000

    betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,10 +8,10 @@ void reverse_array(int *a, int n)
 {
 	int i;
 
-	for (i=0; i < n / 2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
-		a[i] += a[n-i-1];
-		a[n-i-1] = a[i] - a[n-i-1];
-		a[i] -= a[n-i-1];
+		a[i] += a[n - i - 1];
+		a[n - i - 1] = a[i] - a[n - i - 1];
+		a[i] -= a[n - i - 1];
 	}
 }
