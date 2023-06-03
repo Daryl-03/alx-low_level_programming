2a0ff38fb4179f53a33ea19495c8f80e21a57a07
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-06-03 01:47:13 +0000

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+
+/**
+* rev_array - reverses the content of an array of integers.
+* @a: array
+* @n: number of elements of the array
+*/
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i=0; i <= n / 2; i++)
+		a[i] = a[n-i-1];
+}
