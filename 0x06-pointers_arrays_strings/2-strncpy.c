34ffb4ad6327d115dea69bdcedcfb1dcdfb50faf
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-06-03 01:04:03 +0000

    strncpy fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,8 +10,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; src[j] != '\0' && j < n; j++)
-		dest[j] = src[j];
+	for (j = 0; dest[j] != '\0'; j++)
+		dest[j] = (j < n) ? src[j] : '\0';
 
 	return (dest);
 }
