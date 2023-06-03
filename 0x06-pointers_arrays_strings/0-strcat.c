a289888ec66a39c604812517bcb2998eb5744268
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-06-03 00:32:06 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+
+/**
+* _strcat - concatenates two strings
+* @dest: string to be appended to
+* @src: string to append
+* Return: pointer to resulting string
+*/
+char *_strcat(char *dest, char *src){
+	int destLength, j;
+
+	for (destLength = 0;dest[destLength] != '\0';destLength++)
+		;
+	for (j=0;j!='\0';j++)
+		dest[destLength] = src[j];
+	dest[destLength] = src[j];
+
+	return (dest);
+}
