d12001b59beb5950c32fda1313dd5c0fb757e65e
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-06-13 22:52:04 +0000

    last

--- 0x09-static_libraries/memset.c
+++ 0x09-static_libraries/memset.c
@@ -15,15 +15,3 @@ char *_memset(char *s, char b, unsigned int n)
 		*(p + i) = value;
 	return (s);
 }
-
-// to create the static library, run the following command:
-// ar -rc libholbertonschool.a *.o
-// to create the index of the static library, run the following command:
-// ranlib libholbertonschool.a
-// to use the static library, run the following command:
-// gcc main.c -L. -lholbertonschool -o quote
-// to create a static library called liball.a from all the .c files that are in the current directory, run the following command:
-// gcc -c *.c
-// ar -rc liball.a *.o
-// to use the static library liball.a, run the following command:
-// gcc main.c -L. -lall -o quote
