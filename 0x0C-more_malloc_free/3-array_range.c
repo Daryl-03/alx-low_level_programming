e3ee7009318cf07f3c549683faa76426ad1eb0e9
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-06-17 00:30:13 +0000

    declaration

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,6 +12,7 @@
 int *array_range(int min, int max)
 {
 	int *arr;
+	int i;
 
 	if (min > max)
 		return (NULL);
@@ -21,7 +22,7 @@ int *array_range(int min, int max)
 	if (arr == NULL)
 		return (NULL);
 
-	for (int i = 0; i <= max - min; i++)
+	for (i = 0; i <= max - min; i++)
 		arr[i] = min + i;
 
 	return (arr);
