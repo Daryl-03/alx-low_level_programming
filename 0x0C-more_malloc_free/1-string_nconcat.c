4c373e1780471ad7a68e21bb31494ed3f6a11f59
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-06-17 00:15:49 +0000

    fix fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len1; i++)
 		str[i] = s1[i];
 
-	for (j = 0; j < len2; j++, i++)
+	for (j = 0; j < n; j++, i++)
 		str[i] = s2[j];
 
 	*(str + i) = '\0';
