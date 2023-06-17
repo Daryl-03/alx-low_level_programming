529353772610debef5327b94d75a7a774f2ab584
Daryl-03 <79231480+Daryl-03@users.noreply.github.com>
2023-06-17 00:28:18 +0000

    mandatory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,21 @@
 *
 * Return: pointer to allocated memory
 */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
+	void *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (!p)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)p + i) = 0;
+
+	return (p);
 }
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* array_range - creates an array of integers
+* @min: minimum value
+* @max: maximum value
+*
+* Return: pointer to new array, NULL if it fails
+*/
+int *array_range(int min, int max)
+{
+	int *arr;
+
+	if (min > max)
+		return (NULL);
+
+	arr = malloc(sizeof(int) * (max - min + 1));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (int i = 0; i <= max - min; i++)
+		arr[i] = min + i;
+
+	return (arr);
+}
